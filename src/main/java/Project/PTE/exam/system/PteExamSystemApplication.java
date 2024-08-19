ddfe82e9a34e026c20aa565b821afeb0728e99f6
ghmash2 <mahmudpavel74@gmail.com>
2024-08-19 22:13:57 +0600

    #test

---
+++ src/main/java/Project/PTE/exam/system/PteExamSystemApplication.java
@@ -0,0 +1,13 @@
+package Project.PTE.exam.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PteExamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PteExamSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/PTE/exam/system/PteExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.PTE.exam.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PteExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
